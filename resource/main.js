5d0b93f050decf129e4bd935a1983582d90bc1a4
Tonny <joselopez111091@gmail.com>
2022-12-29 17:42:46 -0300

    Funcion para LocalStorage

--- resource/main.js
+++ resource/main.js
@@ -371,66 +371,59 @@ elementfilter5.onclick = () =>{
     filtrobankHSBC.classList.remove('noneview')
 }
 
-/* constantes para los select del formulario de simulacion */
-
-const marcas = ["CHEVROLET","CITROEN","FIAT","DODGE","FORD","PEUGEOT","RENAULT","VOLKSWAGEN"];
-const modelosCH = ["CRUZE","EQUINOX","S-10 PICK-UP","TRAILBLAZER"];
-const modelosCI = ["C4 CACTUS","C4 LOUNGE","C5 AIRCROSS","C-ELYSÉE"];
-const modelosFI = ["ARGO","CRONOS","MOBI","TORO"];
-const modelosDG = ["JOURNEY"];
-const modelosFO = ["BRONCO SPORT","ECOSPORT KD","FIESTA KD","FOCUS KD","KA","RANGER PICK-UP"];
-const modelosPE = ["208","308","408","2008"];
-const modelosRE = ["ALASKAN PICK-UP","CAPTUR","DUSTER","DUSTER OROCH PICK-UP"];
-const modelosVW = ["AMAROK PICK-UP","NIVUS","GOL TREND","GOLF","TAOS"];
+/* Objetos vehiculos, que contiene las marcas y modelos */
 
 const vehiculos = {
     CHEVROLET : {
-        Cruze : ["4P 1.8 LT","4P 1.8 LTZ","4P 1.8 LTZ AT","4P 2.0 DSL LT AT","4P 2.0 DSL LTZ AT","4P 1.4 T LT MT","4P 1.4 T LTZ MT","4P 1.4 T LTZ AT","4P 1.4 T LTZ PLUS AT","4P 1.4 T MIDNIGHT 6AT","4P 1.4 T PREMIER 6AT","5P 1.8 LT,5P 1.8 LTZ","5P 1.8 LTZ AT","5P 2.0 DSL LTZ AT","5P 1.4 T LT MT","5P 1.4 T LTZ MT","5P 1.4 T LTZ AT","5P 1.4 T LTZ PLUS AT","5P 1.4 T PREMIER AT","5P 1.4 T PREMIER 6AT"],
-        Equinox: ["5P 1.5 T 6AT LT 2WD","5P 1.5 T 6AT PREMIER AWD","5P 1.5 T 6AT RS 2WD"],
+        CRUZE : ["4P 1.8 LT","4P 1.8 LTZ","4P 1.8 LTZ AT","4P 2.0 DSL LT AT","4P 2.0 DSL LTZ AT","4P 1.4 T LT MT","4P 1.4 T LTZ MT","4P 1.4 T LTZ AT","4P 1.4 T LTZ PLUS AT","4P 1.4 T MIDNIGHT 6AT","4P 1.4 T PREMIER 6AT","5P 1.8 LT,5P 1.8 LTZ","5P 1.8 LTZ AT","5P 2.0 DSL LTZ AT","5P 1.4 T LT MT","5P 1.4 T LTZ MT","5P 1.4 T LTZ AT","5P 1.4 T LTZ PLUS AT","5P 1.4 T PREMIER AT","5P 1.4 T PREMIER 6AT"],
+        EQUINOX: ["5P 1.5 T 6AT LT 2WD","5P 1.5 T 6AT PREMIER AWD","5P 1.5 T 6AT RS 2WD"],
         S10: ["CS 2.8 CTDI LS 4X2 CV180","CS 2.8 CTDI LS 4X4 CV180","DC 2.8 CTDI LS 4X2 CV180","DC 2.8 CTDI LT 4X2 CV180","DC 2.8 CTDI LTZ 4X2 CV180","DC 2.8 CTDI LS 4X4 CV180","DC 2.8 CTDI LT 4X4 CV180","DC 2.8 CTDI LTZ 4X4 AT CV180","DC 2.8 TD HC 4X4 AT"],
         TRAILBLAZER: ["5P 2.8 CTDI 4X4 LT AT","5P 2.8 CTDI 4X4 LTZ AT","5P 2.8 TD 4X4 PREMIER 7A 6AT"]
     },
-    CITROEN : {
+    CITROEN: {
         C4CACTUS: ["5P 1.2 T 6AT","5P 1.6 VTI 115 LIVE","5P 1.6 VTI 115 FEEL","5P 1.6 VTI 115 FEEL PACK","5P 1.6 VTI 115 FEEL PACK EAT6","5P 1.6 VTi 115 ORIGINS","5P 1.6 THP 165 SHINE EAT6","5P 1.6 THP 165 SHINE EAT6 BITONO","5P 1.6 VTI 115 C-SERIES","5P 1.6 VTI 115 C-SERIES EAT6","5P 1.6 VTI 115 RIP CURL","5P 1.6 VTI 115 RIP CURL  EAT6"],
-        C4LOUNG : ["4P 1.6 VTi 115 LIVE AM19","4P 1.6 THP 165 MT6 FEEL AM19","4P 1.6 THP 165 AT6 FEEL AM19","4P 1.6 THP 165 MT6 FEEL PACK AM19","4P 1.6 THP 165 AT6 SHINE AM19","4P 1.6 HDi 115 MT6 FEEL PACK AM19","4P 1.6 HDi 115 MT6 ORIGINS"], 
+        C4LOUNGE : ["4P 1.6 VTi 115 LIVE AM19","4P 1.6 THP 165 MT6 FEEL AM19","4P 1.6 THP 165 AT6 FEEL AM19","4P 1.6 THP 165 MT6 FEEL PACK AM19","4P 1.6 THP 165 AT6 SHINE AM19","4P 1.6 HDi 115 MT6 FEEL PACK AM19","4P 1.6 HDi 115 MT6 ORIGINS"], 
         C5AIRCROSS:["5P 1.6 THP 165CV EAT6 FELL PACK"],
         CELYSEE : ["4P 1.6 VTi 115 LIVE","4P 1.6 VTi 115 FELL","4P 1.6 VTi 115 6AT SHINE","4P 1.6 HDi 92 FEEL"]
+    },
+    FIAT:{
+        VARGO: ["5P 1.3 DRIVE GSE","5P 1.3 DRIVE GSE PACK CONECTIVIDAD","5P 1.8 PRECISION","5P 1.8 PRECISION PACK PREMIUM","5P 1.8 PRECISION PACK TECNHOLOGY","5P 1.8 PRECISION 6AT","5P 1.8 PRECISION PACK PREMIUM 6AT","5P 1.8 PRECISION PACK TECNHOLOGY 6AT","5P 1.8 HGT"],
+        CRONOS: ["4P 1.3 DRIVE GSE","4P 1.3 DRIVE GSE PACK CONECTIVIDAD","4P 1.8 PRECISION","4P 1.8 PRECISION PACK PREMIUM","4P 1.8 PRECISION PACK TECNHOLOGY","4P 1.8 PRECISION AT","4P 1.8 HGT PACK"],
+        MOBI: ["5P 1.0 EASY","5P 1.0 EASY TOP","5P 1.0 EASY TOP LIVE ON","5P 1.0 WAY","5P 1.0 WAY LIVE ON","5P 1.0 LIKE"],
+        TORO: ["DC 1.8 FREEDOM 4X2 6AT PACK SEG","DC 1.8 FREEDOM 4X2 6AT PACK SEG CHROME","DC 2.0 TD FREEDOM 4X2 6MT","DC 2.0 TD FREEDOM 4X4 6MT","DC 2.0 TD FREEDOM 4X4 6MT PACK XTREME","DC 2.0 TD FREEDOM 4X4 9AT","DC 2.0 TD FREEDOM 4X4 9AT PACK SEG","DC 2.0 TD FREEDOM 4X4 9AT NEW HOLLAN","DC 2.0 TD VOLCANO 4X4 9AT","DC 2.0 TD BLACKJACK 4X4 9AT","DC 2.0 TD VOLCANO 4X4 9AT PACK PREMIUM","DC 2.0 TD RANCH 4X4 9AT","DC 1.3 T VOLCANO 4X2 6AT","DC 2.0 16V TD MULTIJET ULTRA 4X4 9AT"]
+    },
+    DODGE: {
+        JOURNEY: ["5P 2.7 SXT AT","5P 2.7 RT 3F AT","5P 2.7 RT 3F AT TC","5P 3.6 RT ATX AWD"]
+    },
+    FORD: {
+        BRONCOSPORT: ["5P 1.5 4WD 8AT BIG BEND","5P 2.0 4WD 8AT WILDTRAK"],
+        ECOSPORTKD: ["5P 1.5 SE","5P 1.5 FREESTYLE","5P 1.5 TITANIUM","5P 1.5 TITANIUM AT","5P 2.0 FREESTYLE AT 4WD","5P 2.0 TITANIUM AT","5P 2.0 STORM 6AT AWD"],
+        FIESTAKD: ["5P 1.6 S PLUS","5P 1.6 SE","5P 1.6 SE AT","5P 1.6 SE PLUS","5P 1.6 SE PLUS AT","5P 1.6 TITANIUM","5P 1.6 TITANIUM AT"],
+        FOCUSKD: ["5P 1.6 S","5P 2.0 SE","5P 2.0 SE PLUS","5P 2.0 SE PLUS AT","5P 2.0 TITANIUM","5P 2.0 TITANIUM AT"],
+        KA : ["5P 1.5 12V S","5P 1.5 12V SE","5P 1.5 12V SE AT","5P 1.5 12V SEL","5P 1.5 12V SEL AT"],
+        RANGER: ["CS 2.5 NAFTA XL 4X2 5MT","CS 2.2 TDCi XL 4X2 6MT","CS 2.2 TDCi XL 4X4 6MT","DC 2.5 NAFTA XL 4X2 5MT","DC 2.5 NAFTA XLT 4X2 5MT","DC 2.2 TDCi XL 4X2 6MT","DC 2.2 TDCi XL 4X4 6MT","DC 2.2 TDCi XLT 4X2 6AT","DC 3.2 TDCi  XLS 4X2 6MT","DC 3.2 TDCi  XLS 4X2 6AT","DC 3.2 TDCi  XLS 4X4 6MT","DC 3.2 TDCi XLT 4X2 6MT","DC 3.2 TDCi XLT 4X2 6AT","DC 3.2 TDCi XLT 4X4 6MT","DC 3.2 TDCi XLT 4X4 6AT","DC 3.2 TDCi LIMITED 4X4 6MT","DC 3.2 TDCi LIMITED 4X4 6AT","DC 3.2 TDCi BLACK EDITION 4X4 6AT","DC 3.2 TDCi FX4 4X4 6AT","DC 2.0 T PANTHER RAPTOR"]
+    },
+    PEUGEOT: {
+        208: ["5P 1.2 LIKE","5P 1.2 T GT 6TIPT","5P 1.2 T GT 6TIPT LINE","5P 1.6 ACTIVE","5P 1.6 ALLURE","5P 1.6 ALLURE TIPT","5P 1.6 FELINE TIPT","5P 1.6 STYLE","5P 1.6 STYLE TIPT","5P 1.6 ROADTRIP","5P 1.6 ROADTRIP TIPT"],
+        308: ["5P 1.6 ALLURE","5P 1.6 THP FELINE","5P 1.6 THP FELINE TIPT","5P 1.6 HDi ALLURE","5P 1.6 HDi FELINE"],
+        408: ["4P 1.6 HDi ALLURE","4P 1.6 HDi FELINE","4P 1.6 THP ALLURE TIPT","4P 1.6 THP FELLINE TIPT"],
+        2008: ["5P 1.6 ACTIVE","5P 1.6 ALLURE","5P 1.6 ALLURE TIPT","5P 1.6 FELINE","5P 1.6 FELINE TIPT","5P 1.6 THP SPORT TIPT"]
+    },
+    RENAULT : {
+        ALASKAN: ["DC 2.3 DCI T 160CV 4X2 CONFORT 6MT","DC 2.3 DCI T 160CV 4X4 CONFORT 6MT","DC 2.3 DCI T 190CV 4X2 EMOTION 6MT","DC 2.3 DCI T 190CV 4X4 EMOTION 6MT","DC 2.3 DCI T 190CV 4X2 INTENS 6MT","DC 2.3 DCI T 190CV 4X4 INTENS 6MT","DC 2.3 DCI T 190CV 4X4 ICONIC 6MT","DC 2.3 DCI T 190CV 4X4 ICONIC 7AT","DC 2.3 DCI T 190CV 4X4 OUTSIDER 7AT"], 
+        CAPTUR: ["5P 1.6 LIFE","5P 1.6 INTENS CVT","5P 1.6 INTENS CVT BOSE","5P 2.0 ZEN","5P 2.0 INTENS"], 
+        DUSTER: ["5P 1.3 T 4X2 OUTSIDER CVT","5P 1.3 T 4X2 ICONIC","5P 1.3 T 4X4 ICONIC","5P 1.6 4X2 ZEN","5P 1.6 4X2 INTENS CVT","5P 1.6 4X2 EXPRESSION","5P 1.6 4X2 DYNAMIQUE","5P 1.6 4X2 CONFORT"], 
+        DUSTEROROCH: ["DC 1.6 DYNAMIQUE","DC 1.6 OUTSIDER","DC 2.0 DYNAMIQUE","DC 2.0 PRIVELEGE","DC 2.0 OUTSIDER PLUS","DC 2.0 DYNAMIQUE 4X4","DC 2.0 OUTSIDER PLUS 4X4","DC 1.6 PROFESIONAL","DC 1.6 SCE EMOTION 114 2WD","DC 1.3 TCE ICONIC 163 2WD CVT","DC 1.3 TCE OUTSIDER 163 4WD"]
+    },
+    VOLKSWAGEN: {
+        AMAROK : ["DC 2.0 TDI 140CV 4X2 TREND","DC 2.0 TDI 180CV 4X2 COMF","DC 2.0 TDI 180CV 4X2 COMF AT","DC 2.0 TDI 180CV 4X2 HIGH","DC 2.0 TDI 180CV 4X2 HIGH AT","DC 2.0 TDI 140CV 4X4 TREND","DC 2.0 TDI 180CV 4X4 COMF","DC 2.0 TDI 180CV 4X4 HIGH","DC 2.0 TDI 180CV 4X4 HIGH AT","DC 3.0 TDI V6 4X4 COMF AT 258CV","DC 3.0 TDI V6 4X4 8AT HIGH 258CV","DC 3.0 TDI V6 4X4 8AT EXTREME 258CV","DC 3.0 TDI V6 4X4 8AT BLACK STYLE 258CV"],
+        NIVUS : ["5P 1.0 TSI 5MT 170","5P 1.0 TSI 6AT COMFORTLINE 200","5P 1.0 TSI 6AT HIGHLINE 200","5P 1.0 TSI 6AT HERO 200"],
+        GOLTREND : ["5P 1.6 HIGHLINE","5P 1.6 HIGHLINE I MOTTION","5P 1.6 TRENDLINE","5P 1.6 TRENDLINE TIPT","5P 1.6 COMF","5P 1.6 COMF TIPT"],
+        GOLF : ["5P 1.4 TSI COMFORTLINE DSG","5P 250 TSI HIGHLINE DSG","5P GTI"],
+        TAOS : ["5P 1.4 TSI 250 COMFORTLINE TIPT","5P 1.4 TSI 250 HIGHLINE TIPT","5P 1.4 TSI 250 HERO TIPT"]
     }
+
 }
-
-const VCRUZE = ["4P 1.8 LT","4P 1.8 LTZ","4P 1.8 LTZ AT","4P 2.0 DSL LT AT","4P 2.0 DSL LTZ AT","4P 1.4 T LT MT","4P 1.4 T LTZ MT","4P 1.4 T LTZ AT","4P 1.4 T LTZ PLUS AT","4P 1.4 T MIDNIGHT 6AT","4P 1.4 T PREMIER 6AT","5P 1.8 LT,5P 1.8 LTZ","5P 1.8 LTZ AT","5P 2.0 DSL LTZ AT","5P 1.4 T LT MT","5P 1.4 T LTZ MT","5P 1.4 T LTZ AT","5P 1.4 T LTZ PLUS AT","5P 1.4 T PREMIER AT","5P 1.4 T PREMIER 6AT"];
-const VEQUINOX = ["5P 1.5 T 6AT LT 2WD","5P 1.5 T 6AT PREMIER AWD","5P 1.5 T 6AT RS 2WD"];
-const VS10 = ["CS 2.8 CTDI LS 4X2 CV180","CS 2.8 CTDI LS 4X4 CV180","DC 2.8 CTDI LS 4X2 CV180","DC 2.8 CTDI LT 4X2 CV180","DC 2.8 CTDI LTZ 4X2 CV180","DC 2.8 CTDI LS 4X4 CV180","DC 2.8 CTDI LT 4X4 CV180","DC 2.8 CTDI LTZ 4X4 AT CV180","DC 2.8 TD HC 4X4 AT"];
-const VTRAILBLAZER = ["5P 2.8 CTDI 4X4 LT AT","5P 2.8 CTDI 4X4 LTZ AT","5P 2.8 TD 4X4 PREMIER 7A 6AT"];
-const VC4CACTUS = ["5P 1.2 T 6AT","5P 1.6 VTI 115 LIVE","5P 1.6 VTI 115 FEEL","5P 1.6 VTI 115 FEEL PACK","5P 1.6 VTI 115 FEEL PACK EAT6","5P 1.6 VTi 115 ORIGINS","5P 1.6 THP 165 SHINE EAT6","5P 1.6 THP 165 SHINE EAT6 BITONO","5P 1.6 VTI 115 C-SERIES","5P 1.6 VTI 115 C-SERIES EAT6","5P 1.6 VTI 115 RIP CURL","5P 1.6 VTI 115 RIP CURL  EAT6"];
-const VC4LOUNG = ["4P 1.6 VTi 115 LIVE AM19","4P 1.6 THP 165 MT6 FEEL AM19","4P 1.6 THP 165 AT6 FEEL AM19","4P 1.6 THP 165 MT6 FEEL PACK AM19","4P 1.6 THP 165 AT6 SHINE AM19","4P 1.6 HDi 115 MT6 FEEL PACK AM19","4P 1.6 HDi 115 MT6 ORIGINS"];
-const VC5AIRCROSS = ["5P 1.6 THP 165CV EAT6 FELL PACK"];
-const VCELYSEE = ["4P 1.6 VTi 115 LIVE","4P 1.6 VTi 115 FELL","4P 1.6 VTi 115 6AT SHINE","4P 1.6 HDi 92 FEEL"];
-const VARGO = ["5P 1.3 DRIVE GSE","5P 1.3 DRIVE GSE PACK CONECTIVIDAD","5P 1.8 PRECISION","5P 1.8 PRECISION PACK PREMIUM","5P 1.8 PRECISION PACK TECNHOLOGY","5P 1.8 PRECISION 6AT","5P 1.8 PRECISION PACK PREMIUM 6AT","5P 1.8 PRECISION PACK TECNHOLOGY 6AT","5P 1.8 HGT"];
-const VCRONOS = ["4P 1.3 DRIVE GSE","4P 1.3 DRIVE GSE PACK CONECTIVIDAD","4P 1.8 PRECISION","4P 1.8 PRECISION PACK PREMIUM","4P 1.8 PRECISION PACK TECNHOLOGY","4P 1.8 PRECISION AT","4P 1.8 HGT PACK"];
-const VMOBI = ["5P 1.0 EASY","5P 1.0 EASY TOP","5P 1.0 EASY TOP LIVE ON","5P 1.0 WAY","5P 1.0 WAY LIVE ON","5P 1.0 LIKE"];
-const VTORO = ["DC 1.8 FREEDOM 4X2 6AT PACK SEG","DC 1.8 FREEDOM 4X2 6AT PACK SEG CHROME","DC 2.0 TD FREEDOM 4X2 6MT","DC 2.0 TD FREEDOM 4X4 6MT","DC 2.0 TD FREEDOM 4X4 6MT PACK XTREME","DC 2.0 TD FREEDOM 4X4 9AT","DC 2.0 TD FREEDOM 4X4 9AT PACK SEG","DC 2.0 TD FREEDOM 4X4 9AT NEW HOLLAN","DC 2.0 TD VOLCANO 4X4 9AT","DC 2.0 TD BLACKJACK 4X4 9AT","DC 2.0 TD VOLCANO 4X4 9AT PACK PREMIUM","DC 2.0 TD RANCH 4X4 9AT","DC 1.3 T VOLCANO 4X2 6AT","DC 2.0 16V TD MULTIJET ULTRA 4X4 9AT"]; 
-const VJOURNEY = ["5P 2.7 SXT AT","5P 2.7 RT 3F AT","5P 2.7 RT 3F AT TC","5P 3.6 RT ATX AWD"];
-const VBRONCOSPORT = ["5P 1.5 4WD 8AT BIG BEND","5P 2.0 4WD 8AT WILDTRAK"];
-const VECOSPORTKD = ["5P 1.5 SE","5P 1.5 FREESTYLE","5P 1.5 TITANIUM","5P 1.5 TITANIUM AT","5P 2.0 FREESTYLE AT 4WD","5P 2.0 TITANIUM AT","5P 2.0 STORM 6AT AWD"];
-const VFIESTAKD = ["5P 1.6 S PLUS","5P 1.6 SE","5P 1.6 SE AT","5P 1.6 SE PLUS","5P 1.6 SE PLUS AT","5P 1.6 TITANIUM","5P 1.6 TITANIUM AT"]; 
-const VFOCUSKD = ["5P 1.6 S","5P 2.0 SE","5P 2.0 SE PLUS","5P 2.0 SE PLUS AT","5P 2.0 TITANIUM","5P 2.0 TITANIUM AT"];
-const VKA = ["5P 1.5 12V S","5P 1.5 12V SE","5P 1.5 12V SE AT","5P 1.5 12V SEL","5P 1.5 12V SEL AT"];
-const VRANGER = ["CS 2.5 NAFTA XL 4X2 5MT","CS 2.2 TDCi XL 4X2 6MT","CS 2.2 TDCi XL 4X4 6MT","DC 2.5 NAFTA XL 4X2 5MT","DC 2.5 NAFTA XLT 4X2 5MT","DC 2.2 TDCi XL 4X2 6MT","DC 2.2 TDCi XL 4X4 6MT","DC 2.2 TDCi XLT 4X2 6AT","DC 3.2 TDCi  XLS 4X2 6MT","DC 3.2 TDCi  XLS 4X2 6AT","DC 3.2 TDCi  XLS 4X4 6MT","DC 3.2 TDCi XLT 4X2 6MT","DC 3.2 TDCi XLT 4X2 6AT","DC 3.2 TDCi XLT 4X4 6MT","DC 3.2 TDCi XLT 4X4 6AT","DC 3.2 TDCi LIMITED 4X4 6MT","DC 3.2 TDCi LIMITED 4X4 6AT","DC 3.2 TDCi BLACK EDITION 4X4 6AT","DC 3.2 TDCi FX4 4X4 6AT","DC 2.0 T PANTHER RAPTOR"];
-const V208 = ["5P 1.2 LIKE","5P 1.2 T GT 6TIPT","5P 1.2 T GT 6TIPT LINE","5P 1.6 ACTIVE","5P 1.6 ALLURE","5P 1.6 ALLURE TIPT","5P 1.6 FELINE TIPT","5P 1.6 STYLE","5P 1.6 STYLE TIPT","5P 1.6 ROADTRIP","5P 1.6 ROADTRIP TIPT"]
-const V308 = ["5P 1.6 ALLURE","5P 1.6 THP FELINE","5P 1.6 THP FELINE TIPT","5P 1.6 HDi ALLURE","5P 1.6 HDi FELINE"];
-const V408 = ["4P 1.6 HDi ALLURE","4P 1.6 HDi FELINE","4P 1.6 THP ALLURE TIPT","4P 1.6 THP FELLINE TIPT"];
-const V2008 =["5P 1.6 ACTIVE","5P 1.6 ALLURE","5P 1.6 ALLURE TIPT","5P 1.6 FELINE","5P 1.6 FELINE TIPT","5P 1.6 THP SPORT TIPT"];
-const VALASKAN = ["DC 2.3 DCI T 160CV 4X2 CONFORT 6MT","DC 2.3 DCI T 160CV 4X4 CONFORT 6MT","DC 2.3 DCI T 190CV 4X2 EMOTION 6MT","DC 2.3 DCI T 190CV 4X4 EMOTION 6MT","DC 2.3 DCI T 190CV 4X2 INTENS 6MT","DC 2.3 DCI T 190CV 4X4 INTENS 6MT","DC 2.3 DCI T 190CV 4X4 ICONIC 6MT","DC 2.3 DCI T 190CV 4X4 ICONIC 7AT","DC 2.3 DCI T 190CV 4X4 OUTSIDER 7AT"];
-const VCAPTUR = ["5P 1.6 LIFE","5P 1.6 INTENS CVT","5P 1.6 INTENS CVT BOSE","5P 2.0 ZEN","5P 2.0 INTENS"];
-const VDUSTER = ["5P 1.3 T 4X2 OUTSIDER CVT","5P 1.3 T 4X2 ICONIC","5P 1.3 T 4X4 ICONIC","5P 1.6 4X2 ZEN","5P 1.6 4X2 INTENS CVT","5P 1.6 4X2 EXPRESSION","5P 1.6 4X2 DYNAMIQUE","5P 1.6 4X2 CONFORT"];
-const VDUSTEROROCH = ["DC 1.6 DYNAMIQUE","DC 1.6 OUTSIDER","DC 2.0 DYNAMIQUE","DC 2.0 PRIVELEGE","DC 2.0 OUTSIDER PLUS","DC 2.0 DYNAMIQUE 4X4","DC 2.0 OUTSIDER PLUS 4X4","DC 1.6 PROFESIONAL","DC 1.6 SCE EMOTION 114 2WD","DC 1.3 TCE ICONIC 163 2WD CVT","DC 1.3 TCE OUTSIDER 163 4WD"];
-const VAMAROK = ["DC 2.0 TDI 140CV 4X2 TREND","DC 2.0 TDI 180CV 4X2 COMF","DC 2.0 TDI 180CV 4X2 COMF AT","DC 2.0 TDI 180CV 4X2 HIGH","DC 2.0 TDI 180CV 4X2 HIGH AT","DC 2.0 TDI 140CV 4X4 TREND","DC 2.0 TDI 180CV 4X4 COMF","DC 2.0 TDI 180CV 4X4 HIGH","DC 2.0 TDI 180CV 4X4 HIGH AT","DC 3.0 TDI V6 4X4 COMF AT 258CV","DC 3.0 TDI V6 4X4 8AT HIGH 258CV","DC 3.0 TDI V6 4X4 8AT EXTREME 258CV","DC 3.0 TDI V6 4X4 8AT BLACK STYLE 258CV"];
-const VNIVUS = ["5P 1.0 TSI 5MT 170","5P 1.0 TSI 6AT COMFORTLINE 200","5P 1.0 TSI 6AT HIGHLINE 200","5P 1.0 TSI 6AT HERO 200"];
-const VGOLTREND = ["5P 1.6 HIGHLINE","5P 1.6 HIGHLINE I MOTTION","5P 1.6 TRENDLINE","5P 1.6 TRENDLINE TIPT","5P 1.6 COMF","5P 1.6 COMF TIPT"];
-const VGOLF = ["5P 1.4 TSI COMFORTLINE DSG","5P 250 TSI HIGHLINE DSG","5P GTI"];
-const VTAOS = ["5P 1.4 TSI 250 COMFORTLINE TIPT","5P 1.4 TSI 250 HIGHLINE TIPT","5P 1.4 TSI 250 HERO TIPT"];
-
 
 /* capturar <SELECT> del html para rellenarlos con las constantes */
 
@@ -459,36 +452,19 @@ function borrarSelect(a, b){
 let Vmarca = Smarca.value;
 let Vmodelo = Smodelo.value;
 
-
 Smarca.onchange = () =>{
     Vmarca = Smarca.value;
-    localStorage.setItem("marca", Vmarca)
-    switch (Vmarca){
-        case "0":
-            CargarSelect(modelosCH, Smodelo);
+    localStorage.setItem("marca", Vmarca);
+    let x = '';
+    let mar = Object.keys(vehiculos);
+    for(let i = 0; i < mar.length; i++){
+        if(Vmarca == i){
+            x = mar[i];
             break;
-        case "1":
-            CargarSelect(modelosCI, Smodelo);
-            break;
-        case "2":
-            CargarSelect(modelosFI, Smodelo);
-            break;
-        case "3":
-            CargarSelect(modelosDG, Smodelo);
-            break;
-        case "4":
-            CargarSelect(modelosFO, Smodelo);
-            break;
-        case "5":
-            CargarSelect(modelosPE, Smodelo);
-            break;
-        case "6":
-            CargarSelect(modelosRE, Smodelo);
-            break;
-        case "7":
-            CargarSelect(modelosVW, Smodelo);
-            break;
+        }
     }
+    let VM = Object.keys(vehiculos[x]);
+    CargarSelect(VM, Smodelo);
 }
 
 Smodelo.onchange = () =>{
@@ -504,7 +480,7 @@ Smodelo.onchange = () =>{
             break;
         }
     }
-    console.log(x)
+    console.log(x);
     let vers = Object.keys(vehiculos[x]);
     for(let i = 0; i < vers.length; i++){
         if(Vmodelo == i){
@@ -520,20 +496,41 @@ Smodelo.onchange = () =>{
 
 
 // LocalStorage
-let n = localStorage.getItem("nombre");
-let a = localStorage.getItem("apellido");
-let d = localStorage.getItem("dni");
-let s = localStorage.getItem("sexo");
-let mc = localStorage.getItem("mcredit");
-let cuot = localStorage.getItem("Cuotas")
-let m = localStorage.getItem("marca")
-let mm = localStorage.getItem("modelo")
 
-nombre.value = n;
-apellido.value = a;
-dni.value = d;
-sexo.value = s;
-mcredit.value = mc;
-selectcuot.value = cuot;
-Smarca.value = m;
+const LS = () =>{
+    if(localStorage.length > 0){
+        let VentanaE = document.getElementById('window-notice');
+        VentanaE.classList.remove('noneview');
+        let Vno = document.getElementById('window-no');
+        let Vsi = document.getElementById('window-si');
+        Vno.onclick = () => {
+        VentanaE.classList.add('noneview');
+        localStorage.clear();
+        }
+        Vsi.onclick = () => {
+        VentanaE.classList.add('noneview');
+        let n = localStorage.getItem("nombre");
+            let a = localStorage.getItem("apellido");
+            let d = localStorage.getItem("dni");
+            let s = localStorage.getItem("sexo");
+            let mc = localStorage.getItem("mcredit");
+            let cuot = localStorage.getItem("Cuotas");
+            let m = parseInt(localStorage.getItem("marca"));
+            let mm = parseInt(localStorage.getItem("modelo"));
+            nombre.value = n;
+            apellido.value = a;
+            dni.value = d;
+            sexo.value = s;
+            mcredit.value = mc;
+            selectcuot.value = cuot;
+            /*Smarca.value = m;
+            let valMarca = Smarca.options[Smarca.selectedIndex].text;
+            let Mod = Object.keys(vehiculos[valMarca]);
+            console.log(Mod)
+            CargarSelect(Mod , Smodelo);*/
+            
+        }
+    }
+}
+
 
